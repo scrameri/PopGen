f6bd6c66f26f8f29b7dea4cd225792c27609802a
Simon Crameri <scrameri@users.noreply.github.com>
2022-08-26 23:59:15 +0200

    Update Tutorial_ml.R

--- Tutorial_ml.R
+++ Tutorial_ml.R
@@ -13,12 +13,16 @@ source("https://raw.githubusercontent.com/scrameri/PopGen/main/src/ml.R")
 
 
 # These wrapper functions enable access to the wide array of classification
-# (and regression) methods, accessed via the packages caret (caret::tune)
-# and tidymodels (tidymodels::tune_grid) maintained by Max Kuhn.
+# (and regression) methods, accessed via tuning with the packages caret
+# (caret::tune) and tidymodels (tune::tune_grid) maintained by Max Kuhn.
 
 # The wrapper functions can be used for a more streamlined execution of model
 # training (hyperparameter tuning), final model fitting (on complete training
-# data), and model evaluation using the confmat() function.
+# data), model evaluation, and prediction of new data.
+
+# There are some additional functions which may be useful for machine learning
+# - split.data (https://github.com/scrameri/PopGen/wiki/split.data)
+# - confmat (pretty confusion matrix)
 
 # Look at these websites for lists of available methods accessible via caret()
 # - http://topepo.github.io/caret/train-models-by-tag.html
@@ -55,17 +59,23 @@ res <- caret(Xtrain = Xtrain[,-5], Ytrain = Ytrain, Xtest = Xtest[,-5], Ytest = 
              method = mod, metric = metric, trControl = control,
              tuneGrid = expand.grid(mtry = c(1:4)), ntree = 500) # pass additional (fixed) arguments via ...
 
+res # or print(res)
+summary(res) # has a print method, more is returned invisibly
+plot(res) # has some useful arguments, see args(plot.caret)
 confmat(pred = res$pred.test, ref = res$args$Ytest)
 
 # neural network (nnet)
 mod <- "nnet"
 modelLookup(model = mod) # size and decay can be tuned
 
-res <- caret(Xtrain = Xtrain[,-5], Ytrain = Ytrain, Xtest = Xtest[,-5], Ytest = Ytest, 
+res2 <- caret(Xtrain = Xtrain[,-5], Ytrain = Ytrain, Xtest = Xtest[,-5], Ytest = Ytest, 
              method = mod, metric = metric, trControl = control,
              tuneGrid = expand.grid(size = c(1:4), decay = c(0.001,0.01,0.1,1.10)))
 
-confmat(pred = res$pred.test, ref = res$args$Ytest)
+res2 # or print(res2)
+summary(res2) # has a print method, more is returned invisibly
+plot(res2) # has some useful arguments, see args(plot.caret)
+confmat(pred = res2$pred.test, ref = res2$args$Ytest)
 
 
 
@@ -91,10 +101,24 @@ workflow <- workflow(preprocessor = rec, spec = mod)
 tuneGrid <- expand.grid(penalty = c(0.001,0.01,0.1,1,10,100))
 
 # run tidytune wrapper function
-res <- tidytune(Xtrain = Xtrain, Ytrain = Ytrain,
-                Xtest = Xtest, Ytest = Ytest,
-                workflow = workflow, tuneGrid = tuneGrid,
-                folds = folds, trControl = trControl,
-                metrics = metrics, metric.best = "accuracy")
+res3 <- tidytune(Xtrain = Xtrain, Ytrain = Ytrain,
+                 Xtest = Xtest, Ytest = Ytest,
+                 workflow = workflow, tuneGrid = tuneGrid,
+                 folds = folds, trControl = trControl,
+                 metrics = metrics, metric.best = "accuracy")
 
-confmat(pred = res$pred.test$.pred_class, ref = res$args$Ytest)
+res3 # or print(res3)
+summary(res3) # has a print method, more is returned invisibly
+plot(res3) # has some useful arguments, see args(plot.caret)
+confmat(pred = res3$pred.test$.pred_class, ref = res3$args$Ytest) # note that tidytune $pred.X are tibbles
+
+
+## Precit new data
+# assume that the test set <res3$args$Xtest> is new data
+predict(res3, res3$args$Xtest) # tibble of factor, same as using type = "class"
+predict(res3, res3$args$Xtest, type = "raw") # factor
+predict(res3, res3$args$Xtest, type = "prob") # posterior probabilities
+pred <- tibble(predict(res3, new_data = res3$args$Xtest, type = "class"),
+               predict(res3, new_data = res3$args$Xtest, type = "prob"),
+               true_class = res3$args$Ytest)
+confmat(pred = pred$.pred_class, ref = pred$true_class, plot.perf = TRUE, plot.heatmap = FALSE)
